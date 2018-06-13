730454cbddbe7545e995ba7ca564c610c7806df7
Wiebe van Breukelen <wiebe.vanbreukelen@student.hu.nl>
2018-06-13 10:58:49 +0200

    Improved documentation

--- boardController/SerialControl.h
+++ boardController/SerialControl.h
@@ -5,30 +5,64 @@
 * Author: Wiebe
 */
 
-
 #ifndef __SERIALCONTROL_H__
 #define __SERIALCONTROL_H__
 
 #include "ServoActuator.h"
 
-
+/**
+ * @brief This class is used to handle serial commands to control the board.
+ * 
+ */
 class SerialControl
 {
-protected:
+  protected:
+	/**
+	 * @brief Servo control objects
+	 * 
+	 */
 	ServoActuator &servoA, &servoB, &servoC;
 
-public:
-	SerialControl(ServoActuator &servoA, ServoActuator &servoB, ServoActuator &servoC) :
-		servoA(servoA),
-		servoB(servoB),
-		servoC(servoC)
-	{};
-	
-	void begin(const int baudrate = 115200);
-	void loop();
-	void resetServos();
-	inline bool waitForBytes(const unsigned int amount, const unsigned int timeout = 20);
+  public:
+	/**
+	 * @brief Construct a new Serial Control object.
+	 * 
+	 * @param servoA Servo A.
+	 * @param servoB Servo B.
+	 * @param servoC Servo C.
+	 */
+	SerialControl(ServoActuator &servoA, ServoActuator &servoB, ServoActuator &servoC) : servoA(servoA),
+																						 servoB(servoB),
+																						 servoC(servoC){};
 
+	/**
+	 * @brief Begin a serial connection on a desired baudrate.
+	 * 
+	 * @param baudrate Serial baudrate.
+	 */
+	void begin(const int baudrate = 115200);
+
+	/**
+	 * @brief Polls for new commands, and handles them.
+	 * 
+	 */
+	void loop();
+
+	/**
+	 * @brief Reset the servos to the reset position.
+	 * 
+	 */
+	void resetServos();
+
+	/**
+	 * @brief Wait for a specified amount of bytes.
+	 * 
+	 * @param amount Desired amount of bytes to receive.
+	 * @param timeout Receive timeout.
+	 * @return true Bytes succesfully received.
+	 * @return false Timeout triggered, bytes are not received.
+	 */
+	inline bool waitForBytes(const unsigned int amount, const unsigned int timeout = 20);
 
 }; //SerialControl
 
--- boardController/ServoActuator.h
+++ boardController/ServoActuator.h
@@ -5,7 +5,6 @@
 * Author: Wiebe
 */
 
-
 #ifndef __SERVOACTUATOR_H__
 #define __SERVOACTUATOR_H__
 
@@ -13,21 +12,62 @@
 
 class ServoActuator
 {
-private:
+  private:
+	/**
+	 * @brief Servo init/reset position.
+	 * 
+	 */
 	const uint8_t resetPos = 110;
+
+	/**
+	 * @brief Servo upper boundary degrees limit.
+	 * 
+	 */
 	const uint8_t servoUpperBoundary = 110;
+
+	/**
+	 * @brief Servo lower boundary degrees limit.
+	 * 
+	 */
 	const uint8_t servoLowerBoundary = 40;
 
+	/**
+	 * @brief Arduino PWM controllable servo object.
+	 * 
+	 */
 	Servo servo;
 
-public:
-	ServoActuator(const int pin) {
+  public:
+	/**
+	 * @brief Construct a new Servo Actuator object
+	 * 
+	 * @param pin Arduino pin.
+	 */
+	ServoActuator(const int pin)
+	{
 		servo.attach(pin);
 	};
 
+	/**
+	 * @brief Move the servo to a desired position.
+	 * 
+	 * @param pos Position.
+	 * @return true Servo successfully moved.
+	 * @return false Servo did not move.
+	 */
 	bool move(const uint8_t pos);
+
+	/**
+	 * @brief Reset the servo to the reset position.
+	 * 
+	 */
 	void reset();
 
+	/**
+	 * @brief Get the current position of the robot.
+	 * 
+	 * @return uint8_t Motor position.
+	 */
 	uint8_t getPosition();
 }; //ServoActuator
 
